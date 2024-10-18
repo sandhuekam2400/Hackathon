d521d9a60c2b758553dd1abb6e4c0b674b016b8e
Hakaluka <prsaryanjain@gmail.com>
2024-10-19 01:35:52 +0530

    Update gamehighlight.py

--- gamehighlight.py
+++ gamehighlight.py
@@ -14,6 +14,7 @@ if not os.path.exists(current_directory+ '/frames'):
 while True:
     # implementing a queue for the screenshots
     
+    
     while keyboard.is_pressed('q') == True:
         screenshot =pyautogui.screenshot('screenshot.png')
         x+=1
